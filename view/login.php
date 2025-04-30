256f3e2ec352a26732c68568c66699969682f56c
RNA <a@b.com>
2025-04-30 15:51:27 +0200

    Ajoute la fonctionalité "Login"

---
+++ view/login.php
@@ -0,0 +1,7 @@
+
+<h2>Connexion</h2>
+<form method="POST" action="/ctrl/login.php">
+    <input type="email" name="email" placeholder="Email" required><br>
+    <input type="password" name="password" placeholder="Mot de passe" required><br>
+    <button type="submit">Se connecter</button>
+</form>
